51ee7ebc0851db37a7258c9c6f08c17293c9cd94
basedryo <126336423+basedryo@users.noreply.github.com>
2023-04-04 20:53:24 +0200

    Update pentago-game.js

--- public/pentago-game.js
+++ public/pentago-game.js
@@ -179,7 +179,7 @@ class GameScene extends Phaser.Scene {
   // Implements client-side connection and data handling
   connect() {
     let flag = 0;
-    this.socket = new WebSocket('wss://pentago.herokuapp.com:443'); //web-socket server socket
+    this.socket = new WebSocket('wss://pentago.herokuapp.com:5500'); //web-socket server socket
 
     this.socket.addEventListener('open', (event) => {
       this.socket.send(JSON.stringify({ type: 'name', name: playername}));
