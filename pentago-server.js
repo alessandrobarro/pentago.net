67bf092a3c3840f8722b91774a6adc47a88ca1d3
basedryo <126336423+basedryo@users.noreply.github.com>
2023-04-04 22:17:37 +0200

    Update pentago-server.js

--- pentago-server.js
+++ pentago-server.js
@@ -27,7 +27,7 @@ const app = express()
 const WebSocket = require('ws');
 
 // Creates a socket
-const server = new WebSocket.Server({ server: URL });
+const server = new WebSocket.Server({ port: 443 });
 
 console.log('[START] Waiting for a connection');
 
